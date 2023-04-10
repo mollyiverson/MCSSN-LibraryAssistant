b2b5b7f74fa7b6c8bcd263c57b955312f0f797c3
Molly <molly_iverson@outlook.com>
2023-04-09 19:13:20 -0700

    Added search filters.

--- FrontEnd/JavaScriptFiles/search.js
+++ FrontEnd/JavaScriptFiles/search.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let currpage = 1;
   var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
   var outputList = document.getElementById("list-output");
-  var placeHldr = '<img src="/FrontEnd/Static(images)/image-not-found-icon.png">';
+  var placeHldr = "/FrontEnd/Static(images)/image-not-found-icon.png";
 
   document.querySelector('.search-form').addEventListener('submit', search);
 
@@ -10,9 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     outputList.innerHTML = "";
     e.preventDefault();
     let searchData = document.getElementById("input").value;
-    // let searchMethod = document.querySelector('input[name="choice"]:checked').value;
     if (searchData.trim() === "" || searchData == null) {
-      displayError();
+      //displayError();
     } else {
       useAPI(currpage, searchData);
     }
@@ -22,14 +21,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // If we want to implement arrows for viewing the next page of book results
   function nextPage() {
     let searchData = document.getElementById("input").value;
-    // let searchMethod = document.querySelector('input[name="choice"]:checked').value;
+    // let searchMethod = document.querySelector('input[name="searchMethod"]:checked').value;
     currPage++;
     useAPI(currPage, searchData);
   }
 
   function previousPage() {
     let searchData = document.getElementById("input").value;
-    // let searchMethod = document.querySelector('input[name="choice"]:checked').value;
+    // let searchMethod = document.querySelector('input[name="searchMethod"]:checked').value;
     currPage--;
     useAPI(currPage, searchData);
   }
@@ -37,8 +36,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   function useAPI(curr, searchData) {
-    //let url = `https://www.googleapis.com/books/v1/volumes?q="${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10
-    url = bookUrl + searchData
+    var url; //= `https://www.googleapis.com/books/v1/volumes?q="${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10
+    var selectedMethod = document.querySelector('input[name="searchMethod"]:checked').value;
+    if (selectedMethod == "isbn") {
+      url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${searchData}&maxResults=10&startIndex=` + (curr - 1) * 10;
+    } else if (selectedMethod == "title") {
+      url = `https://www.googleapis.com/books/v1/volumes?q=intitle:"${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10;
+
+    } else if (selectedMethod == "author") {
+      url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:"${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10;
+
+    } else if (selectedMethod == "subject") {
+      url = `https://www.googleapis.com/books/v1/volumes?q=subject:"${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10;
+    } else {
+      // default
+      url = bookUrl + searchData;
+    }
     $.ajax({
       url: url,
       dataType: "json",
@@ -46,7 +59,12 @@ document.addEventListener('DOMContentLoaded', function () {
       },
       success: function (response) {
         $(".book-list").css("visibility", "visible");
-        displayResults(response);
+        let bookNotFound = response.totalItems === 0;
+        if (bookNotFound) {
+          outputList.innerHTML += "<h1>No results found</h1>";
+        } else {
+          displayResults(response);
+        }
       },
       error: function () {
         alert("Error - try again!");
@@ -54,49 +72,92 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  // Will need to make code more unique
   function displayResults(response) {
     for (var i = 0; i < response.items.length; i += 2) {
+      // book 1
+
       item = response.items[i];
       title1 = item.volumeInfo.title;
       author1 = item.volumeInfo.authors;
+
+      // if no author
+      if (author1 == undefined) {
+        author1 = "";
+      }
       publisher1 = item.volumeInfo.publisher;
       bookLink1 = item.volumeInfo.previewLink;
-      bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
-      bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
+      bookID = item.id;
 
-      item2 = response.items[i + 1];
-      title2 = item2.volumeInfo.title;
-      author2 = item2.volumeInfo.authors;
-      publisher2 = item2.volumeInfo.publisher;
-      bookLink2 = item2.volumeInfo.previewLink;
-      bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
-      bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr;
+      // if no isbn
+      if (item.volumeInfo.industryIdentifiers && item.volumeInfo.industryIdentifiers.length >= 2) {
+        bookIsbn = item.volumeInfo.industryIdentifiers[0].identifier; // isbn 13 not 10
+      } else {
+        bookIsbn = "ISBN not available";
+      }
 
-      // in production code, item.text should have the HTML entities escaped.
-      outputList.innerHTML += '<div class="row mt-4">' +
-        formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-        formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
-        '</div>';
+      // if no image
+      if (item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail) {
+        bookImg1 = item.volumeInfo.imageLinks.thumbnail;
+      } else if (placeHldr) {
+        bookImg1 = placeHldr;
+      } else {
+        bookImg1 = "image not available";
+      }
 
+      if (i + 1 < response.items.length) {
+        // book 2
+        item2 = response.items[i + 1];
+        title2 = item2.volumeInfo.title;
+        author2 = item2.volumeInfo.authors;
+        if (author2 == undefined) {
+          author2 = "";
+        }
+        publisher2 = item2.volumeInfo.publisher;
+        bookLink2 = item2.volumeInfo.previewLink;
+        bookID2 = item2.id;
+
+        // if no isbn
+        if (item2.volumeInfo.industryIdentifiers && item2.volumeInfo.industryIdentifiers.length >= 2) {
+          bookIsbn2 = item2.volumeInfo.industryIdentifiers[0].identifier;
+        } else {
+          bookIsbn2 = "ISBN not available";
+        }
+
+        // if no image
+        if (item2.volumeInfo.imageLinks && item2.volumeInfo.imageLinks.thumbnail) {
+          bookImg2 = item2.volumeInfo.imageLinks.thumbnail;
+
+        } else if (placeHldr) {
+          bookImg2 = placeHldr;
+        } else {
+          bookImg2 = "image not available";
+        }
+
+        outputList.innerHTML += '<div class="row mt-4">' +
+          formatOutput(bookImg1, title1, author1, bookID, bookLink1, bookIsbn) +
+          formatOutput(bookImg2, title2, author2, bookID2, bookLink2, bookIsbn2) +
+          '</div>';
+
+      } else {
+        outputList.innerHTML += '<div class="row mt-4">' +
+          formatOutput(bookImg1, title1, author1, bookID, bookLink1, bookIsbn) +
+          '</div>';
+      }
     }
-
   }
 
-  // Will need to make code more unique
-  function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
-    var viewUrl = 'book.html?isbn=' + bookIsbn; //constructing link for bookviewer
+  function formatOutput(bookImg, title, author, bookID, bookLink, bookIsbn) {
+    var viewUrl = 'book.html?isbn=' + bookID; //constructing link for bookviewer
     var htmlCard = `<div class="col-lg-6">
           <div class="card" style="">
             <div class="row no-gutters">
-              <div class="col-md-4">
+              <div class="col-md-6">
                 <img src="${bookImg}" class="card-img" alt="...">
               </div>
-              <div class="col-md-8">
+              <div class="col-md-5">
                 <div class="card-body">
                   <h5 class="card-title">${title}</h5>
-                  <p class="card-text">Author: ${author}</p>
-                  <p class="card-text">Publisher: ${publisher}</p>
+                  <p class="card-text">${author}</p>
                   <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
                 </div>
               </div>
@@ -107,7 +168,46 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function displayError() {
-    alert("Search term can not be empty!")
+    alert("Search term can not be empty!");
   }
 
+
+  // SIDE BAR LOGIC
+
+  // Changes the color of the filter button when it is selected
+  const radioButtons = document.querySelectorAll('input[name="searchMethod"]');
+  const containers = document.querySelectorAll('.sidebar-container');
+
+  // When the sidebar container or button is clicked, change the color of the container
+  containers.forEach(container => {
+    const label = container.querySelector('label');
+    container.addEventListener('click', () => {
+      button.checked = true;
+      containers.forEach(c => c.classList.remove('selected'));
+      container.classList.add('selected');
+    });
+    label.addEventListener('click', () => {
+      button.checked = true;
+      containers.forEach(c => c.classList.remove('selected'));
+      container.classList.add('selected');
+    });
+  });
+
+  radioButtons.forEach(button => {
+    button.addEventListener('change', () => {
+      containers.forEach(container => {
+        if (button.id === container.id.replace("-container", "")) {
+          container.classList.add('selected');
+        } else {
+          container.classList.remove('selected');
+        }
+      });
+    });
+  });
+
+  // Clicks the default button first  
+  const radioButton = document.querySelector('#default');
+  radioButton.click();
+
 });
+
