cf54018dcde303b2bdb297d424fb69dbcb0563d8
Molly <molly_iverson@outlook.com>
2023-04-15 17:13:00 -0700

    Added back/forward buttons for searching

--- FrontEnd/JavaScriptFiles/search.js
+++ FrontEnd/JavaScriptFiles/search.js
@@ -1,43 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let currpage = 1;
+  const paginationWrapper = document.getElementById("pagination-wrapper");
+  paginationWrapper.style.visibility = "hidden";
   var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
   var outputList = document.getElementById("list-output");
   var placeHldr = "/FrontEnd/Static(images)/image-not-found-icon.png";
+  var booksNext = true;
+  let currpage = 1;
+  var searchData = ""; // keeps track of the last search
+  var selectedMethod = "";
 
-
+  // Searchs for a book when the user inputs data
   document.querySelector('.search-form').addEventListener('submit', search);
 
+  // Calls functions when the next page, previous page, and back to the start buttons are clicked
+  document.querySelector('.fa-circle').addEventListener('click', reset);
+  document.querySelector('.fa-angle-right').addEventListener('click', nextPage);
+  document.querySelector('.fa-angle-left').addEventListener('click', previousPage);
+
+  // Calls the API with the search data
   function search(e) {
+    paginationWrapper.style.visibility = "hidden";
+    booksNext = true;
     outputList.innerHTML = "";
     e.preventDefault();
-    let searchData = document.getElementById("input").value;
+    searchData = document.getElementById("input").value;
     if (searchData.trim() === "" || searchData == null) {
       //displayError();
     } else {
-      useAPI(currpage, searchData);
+      selectedMethod = document.querySelector('input[name="searchMethod"]:checked').value;
+      useAPI(currpage, searchData, selectedMethod);
     }
-
   }
 
-  // If we want to implement arrows for viewing the next page of book results
+  // Goes to the next 10 results from the API
   function nextPage() {
-    let searchData = document.getElementById("input").value;
-    // let searchMethod = document.querySelector('input[name="searchMethod"]:checked').value;
-    currPage++;
-    useAPI(currPage, searchData);
+    if (booksNext) {
+      window.scroll(0, 0);
+      paginationWrapper.style.visibility = "hidden";
+      currpage++;
+      outputList.innerHTML = "";
+      useAPI(currpage, searchData, selectedMethod);
+    }
   }
 
+  // Goes to the starting 10 results from the API
+  function reset() {
+    window.scroll(0, 0);
+    currpage = 1;
+    paginationWrapper.style.visibility = "hidden";
+    outputList.innerHTML = "";
+    useAPI(currpage, searchData, selectedMethod);
+  }
+
+  // Goes to the previous 10 results from the API
   function previousPage() {
-    let searchData = document.getElementById("input").value;
-    // let searchMethod = document.querySelector('input[name="searchMethod"]:checked').value;
-    currPage--;
-    useAPI(currPage, searchData);
+    if (currpage > 1) {
+      window.scroll(0, 0);
+      paginationWrapper.style.visibility = "hidden";
+      booksNext = true;
+      currpage--;
+      outputList.innerHTML = "";
+      useAPI(currpage, searchData, selectedMethod);
+    }
   }
 
-
-
-  function useAPI(curr, searchData) {
-    var selectedMethod = document.querySelector('input[name="searchMethod"]:checked').value;
+  // Gets data from Google Books API
+  function useAPI(curr, searchData, selectedMethod) {
     if (selectedMethod == "isbn") {
       url = bookUrl + `isbn:${searchData}&maxResults=10&startIndex=` + (curr - 1) * 10;
     } else if (selectedMethod == "title") {
@@ -50,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
       url = bookUrl + `subject:"${searchData}"&maxResults=10&startIndex=` + (curr - 1) * 10;
     } else {
       // default
-      url = bookUrl + searchData;
+      url = bookUrl + searchData + "&maxResults=10&startIndex=" + (curr - 1) * 10;
     }
     $.ajax({
       url: url,
@@ -61,9 +89,11 @@ document.addEventListener('DOMContentLoaded', function () {
         $(".book-list").css("visibility", "visible");
         let bookNotFound = response.totalItems === 0;
         if (bookNotFound) {
+          booksNext = false;
           outputList.innerHTML += "<h1>No results found</h1>";
         } else {
           displayResults(response);
+          paginationWrapper.style.visibility = "visible";
         }
       },
       error: function () {
@@ -72,41 +102,42 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-function displayResults(response) {
-  for (var i = 0; i < response.items.length; i += 2) {
-    // create a new row for each pair of cards
-    var row = document.createElement("div");
-    row.classList.add("row");
+  // Displays the books as boxes on the screen
+  function displayResults(response) {
+    for (var i = 0; i < response.items.length; i += 2) {
+      // create a new row for each pair of cards
+      var row = document.createElement("div");
+      row.classList.add("row");
 
-    // card 1
-    var item = response.items[i];
-    var title1 = item.volumeInfo.title;
-    var author1 = item.volumeInfo.authors || "";
-    var bookID = item.id;
-    var bookImg1 = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ? item.volumeInfo.imageLinks.thumbnail : placeHldr || "image not available";
-    var card1 = formatOutput(bookImg1, title1, author1, bookID);
-    var col1 = document.createElement("div");
-    col1.classList.add("col-lg-6");
-    col1.appendChild(card1);
-    row.appendChild(col1);
+      // card 1
+      var item = response.items[i];
+      var title1 = item.volumeInfo.title;
+      var author1 = item.volumeInfo.authors || "";
+      var bookID = item.id;
+      var bookImg1 = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ? item.volumeInfo.imageLinks.thumbnail : placeHldr || "image not available";
+      var card1 = formatOutput(bookImg1, title1, author1, bookID);
+      var col1 = document.createElement("div");
+      col1.classList.add("col-lg-6");
+      col1.appendChild(card1);
+      row.appendChild(col1);
 
-    if (i + 1 < response.items.length) {
-      // card 2
-      var item2 = response.items[i + 1];
-      var title2 = item2.volumeInfo.title;
-      var author2 = item2.volumeInfo.authors || "";
-      var bookID2 = item2.id;
-      var bookImg2 = item2.volumeInfo.imageLinks && item2.volumeInfo.imageLinks.thumbnail ? item2.volumeInfo.imageLinks.thumbnail : placeHldr || "image not available";
-      var card2 = formatOutput(bookImg2, title2, author2, bookID2);
-      var col2 = document.createElement("div");
-      col2.classList.add("col-lg-6");
-      col2.appendChild(card2);
-      row.appendChild(col2);
+      if (i + 1 < response.items.length) {
+        // card 2
+        var item2 = response.items[i + 1];
+        var title2 = item2.volumeInfo.title;
+        var author2 = item2.volumeInfo.authors || "";
+        var bookID2 = item2.id;
+        var bookImg2 = item2.volumeInfo.imageLinks && item2.volumeInfo.imageLinks.thumbnail ? item2.volumeInfo.imageLinks.thumbnail : placeHldr || "image not available";
+        var card2 = formatOutput(bookImg2, title2, author2, bookID2);
+        var col2 = document.createElement("div");
+        col2.classList.add("col-lg-6");
+        col2.appendChild(card2);
+        row.appendChild(col2);
+      }
+
+      outputList.appendChild(row);
     }
-
-    outputList.appendChild(row);
   }
-}
 
   function formatOutput(bookImg, title, author, bookID) {
     var htmlCard = `<div class="col-lg-11">
