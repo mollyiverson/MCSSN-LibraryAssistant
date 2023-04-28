e7d2c73ad591db0a3fa5bf2bdba5fca17efc4383
skyllare <skyllarautumn@gmail.com>
2023-04-28 11:38:57 -0700

    index page change and functionality for signup working

--- FrontEnd/JavaScriptFiles/signup.js
+++ FrontEnd/JavaScriptFiles/signup.js
@@ -1,96 +1,24 @@
-/* 
-document.getElementById('signfirst-btn').onclick = function() 
-{
-  
-  /*var email = document.getElementById('Email').value;
-  var password = document.getElementById('Password').value;
-  var username = document.getElementById('Username').value;
-  alert(email); 
-  const { Client } = require('pg')
-  const client = new Client({
-    user: 'caitlingraves',
-    password: 'Duke2023',
-    host: 'library.postgres.database.azure.com',
-    port: 5432,
-    database: 'postgres',
-    ssl:true,
-    authentication: {
-      type: 'default'
-   },
-    set: {sslmode: 'require'},
-  })
-  client.connect(function(err){
-    if (err) throw err;
-    console.log("Connected!");
-
-    //client.query("INSERT INTO Profile VALUES ('" + email + "', '1111111112', '" + password + "', '" + user +"', 'testFirst1', 'testLast1', '0002');", function(err, result) 
-    client.query("INSERT INTO Profile VALUES ('testingsingup', '1111111112', 'testingsingup', 'testingsingup', 'testFirst1', 'testLast1', '0002');", function(err, result) 
-    {
-      if (err) throw err;
-      client.end();
-    });
-});
-}
-*/
-/*
-const { Client } = require('pg')
-//const mysql = require('mysql')
-//const dotenv = require('dotenv')
-//dotenv.config()
-const client = new Client({
-  user: 'caitlingraves',
-  password: 'Duke2023',
-  host: 'library.postgres.database.azure.com',
-  port: 5432,
-  database: 'postgres',
-  ssl:true,
-  authentication: {
-    type: 'default'
-  },
-  set: {sslmode: 'require'},
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  client.query("SELECT * FROM profile;", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-    //Note: You need client.end() so that the code can end.
-    //I assume in our code we will want client.end() after all
-    //queries are called.
-    client.end()
-  });
-  
-});*/
-
-      //Creates a user with the next id using form answers:
-      function createUser() {
-        //Gets user typed values:
-        var email = document.getElementById("email").value;
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var id = "0000";
-        //Generates id:
-        getLargestID(function(id) {
-          console.log(id);
-          var xhr = new XMLHttpRequest();
-          //Runs the script:
-          xhr.open("POST", "http://localhost:3000/createUser", true);
-          xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-          //Set payload and send in string format:
-          var payload = ({ email: email, username: username, password: password, id:id });
-          xhr.send(JSON.stringify( payload ));
-          xhr.onreadystatechange = function() {
-            //Checks the request:
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-              //Success:
-              if (xhr.status === 200) {
-                console.log("Inserted");
-              } else {
-                //Failure:
-                console.error('Failed to make POST request:', xhr.status);
-              }
-            }
-          };  
+document.addEventListener('DOMContentLoaded', function () {app.post('/createUser', (req, res) => {
+    //Gets information from html file:
+    const { email, username, password, id } = req.body;
+    //Display information:
+    console.log(req.body);
+    res.send("User created successfully.");
+    //Create the query:
+    const query = "INSERT INTO Profile (email, username, password, id) VALUES ('" + email + "', '" + username + "', '" + password + "', '" + id + "');";
+    //Query:
+    client.query(query)
+        //Successful:
+        .then(result => {
+            console.log('INSERTED: ' + query);
+        })
+        //Error occurred:
+        .catch(err => {
+            console.error('Failed to execute query:', err);
+            res.status(500).send('Failed to execute query');
         });
-      };
+        //Insert into general table:
+    const generalQuery = "INSERT INTO GeneralUser (ID) VALUES ('" + userID + "');";
+    client.query(generalQuery);
+});
+});
